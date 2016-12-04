1c45a6e24ffa386e684ba9f4b13cf66b2c08fd3c
Richard David Conover <Princeton.Tau@gmail.com>
2016-12-03 20:53:49 -0500

    Add logic for ZIP and TXT file

--- shell/dbSyncFiles_1x0.sh
+++ shell/dbSyncFiles_1x0.sh
@@ -3,18 +3,24 @@
 ProgramName="#!/bin/bash"
 
 ProgramName="dbSyncFiles_1x0.sh"
+
+# Find  the specifid GPS Files To Add 
 # Richard David Conover
 #  11/30/2016
 
-
-# Environment has the 
-TrailsDbRoot=${TRLS_DBF_ROOT}
+# Environment has the path to the source files and database 
+# All of the input files are in the path - ${TRLS_FTP_ROOT}
 TrailsGpsSrcFiles=${TRLS_FTP_ROOT}
 
+# The GPS Capture database is 
+TrailsDbRoot=${TRLS_DBF_ROOT}
+
+
+# Show datasets if noisy
 if [ ${TRLS_NOISY} = 12 ]
 then
-echo "DbRoot  =${TrailsDbRoot}"
-echo "FtpRoot =${TrailsGpsSrcFiles}"
+echo "DbRoot  =${TRLS_DBF_ROOT}"
+echo "FtpRoot =${TRLS_FTP_ROOT}"
 ls -al ${TrailsDbRoot}
 ls -al ${TrailsGpsSrcFiles}
 fi
@@ -25,19 +31,33 @@ fi
 # Richard David Conover
 #  11/30/2016
 
-
-
 # Move the specifid GPS File 
-NameFilter=${1-"*.txt"}
-RootPath=${2-"/home/richard/trails/db/"}
-OutPath=${3-"/home/richard/trails/tmp"}
+#NameFilter=${1-"*.txt"}
+#RootPath=${2-"/home/richard/trails/db/"}
+#OutPath=${3-"/home/richard/trails/tmp"}
+
 Ret=9
 
-
-
-MyInputTxtFiles=`ls ${TrailsGpsSrcFiles}/*.txt`
+# - - - - - - - - - - - - - - - - - - - - - - - -
 MyInputZipFiles=`ls ${TrailsGpsSrcFiles}/*.zip`
-MyInputFiles=`echo "${MyInputTxtFiles} ${MyInputZipFiles}"`
+
+if [ ${TRLS_SYNC_TXT} = 1 ]
+then
+MyInputFiles=`ls ${TrailsGpsSrcFiles}/*.txt`
+fi
+
+if [ ${TRLS_SYNC_ZIP} = 1 ]
+then
+MyInputFiles+=`ls ${TrailsGpsSrcFiles}/*.zip`
+fi
+
+
+
+
+
+
+
+
 
 
 #echo ${MyInputFiles}
