a33345bf77c747430965d15657548966c1a4ef82
Richard David Conover <Princeton.Tau@gmail.com>
2016-12-07 21:34:05 -0500

    Add cron changes.

---
+++ cgi-bin/example-bash.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "Content-type: text/html"
+echo ''
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+echo 'CGI Bash Example --------- Richard'
+
+
